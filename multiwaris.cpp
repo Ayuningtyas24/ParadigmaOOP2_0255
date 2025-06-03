ad3ff5d71a3fade7e15c06a5651cb332d758d126
Ayuningtyas24 <adyahseptiani@gmail.com>
2025-06-03 10:23:52 +0700

    menambahkan class pekerja

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -6,8 +6,19 @@ class orang
 public:
     int umur;
 
-    orang(int pUmur) : umur(pUmur)
+    orang(int pUmur) : 
+        umur(pUmur)
     {
         cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+    }
+};
+
+class pekerja : virtual public orang
+{
+public:
+    pekerja(int pUmur) : 
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
     }
 };

f6649d296ab12695e08ba0b4b64ebb23a16e4b8d
Ayuningtyas24 <adyahseptiani@gmail.com>
2025-06-03 10:31:50 +0700

    menambahkan class budi untuk mendeklarasikan pekerja dan pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -6,10 +6,10 @@ class orang
 public:
     int umur;
 
-    orang(int pUmur) : 
-        umur(pUmur)
+    orang(int pUmur) : umur(pUmur)
     {
-        cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+        cout << "orang dibuat dengan umur" << umur << "\n"
+             << endl;
     }
 };
 
@@ -23,13 +23,24 @@ public:
     }
 };
 
-class pelajar : virtual public orang 
+class pelajar : virtual public orang
 {
 public:
-
-    pelajar(int pUmur) :
-        orang(pUmur) 
+    pelajar(int pUmur) : 
+        orang(pUmur)
     {
-        cout <<"pelajar dibuat\n" << endl;
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
+class budi : public pekerja, public pelajar
+{
+public:
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur),
+        orang(pUmur) // hal ini dapat dilakukan jika menggunakan virtual
+    {
+        cout << "Budi dibuat\n" << endl;
     }
 };
